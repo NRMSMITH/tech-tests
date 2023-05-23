19670b60b4cf54fe1fae7bfcc53ea1b89b28d427
Niamh Smith <niamh@Niamhs-MacBook-Pro.local>
2023-05-23 11:17:37 +0100

    allow cross origin resource sharing for reading-books-api

--- BEND/reading-books-api/api/app.js
+++ BEND/reading-books-api/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 
 const {getBooks} = require('./controllers/books.controller')
 const {getBookshops, getBookshopById} = require('./controllers/bookshops.controller')
 const {allPathErrors, handleCustomErrors, handleInternalServerErrors} = require('./controllers/errors.controller')
 
 
+app.use(cors());
 
 app.get('/api/books', getBooks)
 
